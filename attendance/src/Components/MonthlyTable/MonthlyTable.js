143b7539e9e63658c76c51e3b8f474aca74749cf
giridhar <giribala2001@gmail.com>
2024-02-15 22:33:33 +0530

    changes

--- attendance/src/Components/MonthlyTable/MonthlyTable.js
+++ attendance/src/Components/MonthlyTable/MonthlyTable.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import "../MonthlyTable/MonthlyTable.css"
+import * as XLSX from 'xlsx';
+import Holiday from "../../Assets/holiday.xlsx"
 
 export default function MonthlyTable() {
     const [data, setData] = useState([]);
+    const [holidays, setHolidays] = useState([]);
+    const [monthHolidays, setMonthHolidays] = useState([])
     const numberofDays = []
     const monthName = sessionStorage.getItem("month")
-    const holidays = [""]
 
     const getData = async () => {
         await axios.post("http://localhost:9000/month", { "month": monthName })
@@ -29,8 +32,56 @@ export default function MonthlyTable() {
     days();
 
     useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(Holiday);
+                const arrayBuffer = await response.arrayBuffer();
+                const binaryString = String.fromCharCode.apply(null, new Uint8Array(arrayBuffer));
+                const workbook = XLSX.read(binaryString, { type: 'binary' });
+                const sheetName = workbook.SheetNames[0];
+                const sheet = workbook.Sheets[sheetName];
+
+                // Parse all cells as text to preserve leading zeros
+                const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1, raw: false, dateNF: 'dd-mm-yy' });
+
+                // Extracting headers (first row) and data (remaining rows)
+                const [headers, ...rows] = jsonData;
+
+                // Creating array of objects with headers as keys and corresponding values from rows
+                const parsedData = rows.map(row =>
+                    headers.reduce((obj, key, index) => {
+                        obj[key] = row[index];
+                        return obj;
+                    }, {})
+                );
+                setHolidays(parsedData)
+                console.log(parsedData)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
         getData();
+
     }, [])
+
+    const getHolidays = () => {
+        const holiday = []
+        const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "octobar", "november", "december"]
+        holidays.filter(val => parseInt(val.Date.substring(3, 5)) === months.indexOf(monthName.toLowerCase()) + 1)
+            .map(value => holiday.push(value))
+        return holiday
+
+    }
+    const getHoliday = getHolidays()
+    setTimeout(() => {
+        console.log(getHoliday.length)
+    }, 1000)
+
 
     return (
         <div className='monthlyTable'>
@@ -55,19 +106,11 @@ export default function MonthlyTable() {
                                 value.leaves_dates.split(`,`).map(x => { return +x }).includes(v) ?
                                     <td key={index} className='leave'>L</td>
                                     :
-                                    <>
-                                        {numberofDays?.map((v, index) =>
-                                            value.leaves_dates.split(`,`).map(x => { return +x }).includes(v) ?
-                                                <td key={index} className='leave'>L</td>
-                                                :
-                                                <td key={index} className='present'>P</td>
-                                        )
-                                        }
-                                    </>
+                                    <td key={index} className='present'>P</td>
                             ))}
-                            <td>{numberofDays.length - value.leaves_dates.split(`,`).map(x => { return +x }).length}</td>
+                            <td>{numberofDays.length - value.leaves_dates.split(`,`).map(x => { return +x }).length - getHoliday.length}</td>
                             <td>{value.leaves_dates.split(`,`).map(x => { return +x }).length}</td>
-                            <td>0</td>
+                            <td>{getHoliday.length}</td>
                         </tr>
                     ))}
                 </tbody>
